3b08b0c684fa3613f68666e1990cd0e8a5df98f9
Faiez Fattah Wisnubrata <faiezfattah2005@gmail.com>
2025-02-26 11:43:39 +0700

    fix

--- components/Primitive/LabeledCheckbox.vue
+++ components/Primitive/LabeledCheckbox.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+const model = defineModel<boolean>()
+</script>
+
 <template>
     <div class="flex flex-row align-center gap-2">
-        <Checkbox id="test" class="bg-brand-white"/>
+        <Checkbox id="test" class="bg-brand-white" v-model="model" />
         <Label for="test" class="mt-1">
             <slot />
         </Label>
--- pages/booking.vue
+++ pages/booking.vue
@@ -4,6 +4,7 @@ interface BookingRequest {
     Name: string | number | undefined,
     Email: string | number | undefined,
     Date: string | undefined,
+    Check: boolean,
     Signature: string | null
 }
 </script>
@@ -13,11 +14,13 @@ const booking = reactive<BookingRequest>({
     Name: undefined,
     Email: undefined,
     Date: undefined,
+    Check: false,
     Signature: null
 })
 function validate() {
-    console.log(booking.Date)
-    if (booking.Name == undefined || booking.Email == undefined || booking.Date == undefined || booking.Signature == null) {
+    // console.log(booking.Date)
+    if (booking.Name == undefined || booking.Email == undefined || 
+    booking.Date == undefined || booking.Signature == null || !booking.Check) {
         console.log("Invalid");
         return;
     }
@@ -36,7 +39,7 @@ function validate() {
             <div class="w-2/3 flex flex-col justify-around">
                 <div>
                     <Label>Name</Label>
-                    <Input id="Name" type="email" v-model="booking.Name" class="rounded-full bg-brand-white border-none text-brand-black"/>
+                    <Input id="Name" type="email" v-model="booking.Name" class="rounded-full bg-brand-white border-none text-brand-black invalid:border-brand-brown"/>
                 </div>
                 
                 <div>
@@ -47,7 +50,7 @@ function validate() {
                 <PrimitiveDatePicker v-model="booking.Date" />
 
 
-                <PrimitiveLabeledCheckbox>
+                <PrimitiveLabeledCheckbox v-model="booking.Check">
                     <p>I Agree with the term and condition</p>
                 </PrimitiveLabeledCheckbox>
 
