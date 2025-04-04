ea5a72303c5c09e20382854a8b7acd74ce9a556e
Faiez Fattah Wisnubrata <faiezfattah2005@gmail.com>
2025-04-04 17:06:33 +0700

    bookings category

--- pages/admin/booking-list.vue
+++ pages/admin/booking-list.vue
@@ -2,53 +2,70 @@
 import { ref } from 'vue';
 import { BookingStatus, type BookingData } from '~/types/BookingData';
 
-const booking = await useFetch<BookingData[]>('/api/bookings', {
-  method: 'GET'
-});
-
-const dummyBookingRef = ref<BookingData[] | null>(booking.data.value)
+const Bookings = ref<BookingData[]>([])
+const ActiveStatus = ref<BookingStatus | undefined>(undefined);
+const isLoading = ref<boolean>(false);
 
 async function HandleDelete(booking: BookingData) {
-  booking.status = BookingStatus.rejected;
-  await $fetch('/api/bookings', {
-    method: 'PATCH',
-    body: booking
-  })
+    booking.status = BookingStatus.rejected;
+    await $fetch('/api/bookings', {
+        method: 'PATCH',
+        body: booking
+    })
 
-  const r = await $fetch<BookingData[]>('/api/bookings', {
-    method: 'GET'
-  }).then(
-      (value) => dummyBookingRef.value = value,
-  )
+    FetchBookings(ActiveStatus.value);
 }
 
 async function HandleAccept(booking: BookingData) {
-  booking.status = BookingStatus.accepted;
-  await $fetch('/api/bookings', {
-    method: 'PATCH',
-    body: booking
-  })
+    booking.status = BookingStatus.accepted;
+    await $fetch('/api/bookings', {
+        method: 'PATCH',
+        body: booking
+    })
 
-  const r = await $fetch<BookingData[]>('/api/bookings', {
-    method: 'GET'
-  }).then(
-      (value) => dummyBookingRef.value = value,
-  )
+    FetchBookings(ActiveStatus.value);
 }
+async function FetchBookings(status?: BookingStatus) {
+    const query = status ? { status: status } : {};
+    ActiveStatus.value = status;
+    isLoading.value = true;
+    try {
+        const r = await $fetch<BookingData[]>('/api/bookings', {
+            method: "GET",
+            query: query
+        }).then(
+            (value) => Bookings.value = value
+        )
+    } 
+    finally {
+        isLoading.value = false;
+    }
+}
+
+FetchBookings();
 </script>
 
 <template>
-  <main class="full-container h-screen overflow-scroll flex flex-col items-center ">
-    <PageCompositNavbarAdminNavbar />
+    <main class="full-container h-screen overflow-scroll flex flex-col items-center ">
+        <h1 class="font-copasetic text-8xl mt-8">ADMIN</h1>
+        <div class="flex gap-4 text-brand-white tracking-[0.2em] mb-4">
+            <p class="font-copasetic text-2xl mb-8" href="/admin/booking-list">BOOKINGS</p>
+        </div>
 
-    <div class="flex flex-col gap-4 w-full">
-      <LazyPrimitiveBookingItem
-          v-for="booking in dummyBookingRef"
-          :key="booking.id"
-          :booking="booking"
-          @delete="HandleDelete"
-          @accept="HandleAccept"
-      />
-    </div>
-  </main>
+        <div class="mb-4 w-full flex gap-2">
+            <PrimitiveButton size="small" @click="() => FetchBookings(BookingStatus.rejected)">Rejected</PrimitiveButton>
+            <PrimitiveButton size="small" @click="() => FetchBookings(BookingStatus.pending)">Pending</PrimitiveButton>
+            <PrimitiveButton size="small" @click="() => FetchBookings(BookingStatus.accepted)">Accepted</PrimitiveButton>
+            <PrimitiveButton size="small" @click="() => FetchBookings()">All</PrimitiveButton>
+        </div>
+
+        <div class="flex flex-col gap-4 w-full">
+            <LazyPrimitiveBookingItem v-if="!isLoading && Bookings?.length > 0" v-for="booking in Bookings" :key="booking.id" :booking="booking"
+                @delete="HandleDelete" @accept="HandleAccept" />
+
+            <p v-if="isLoading">Loading...</p>
+            
+            <p v-if="!isLoading && Bookings.length == 0">No Bookings Found</p>
+        </div>
+    </main>
 </template>
--- server/api/bookings/index.get.ts
+++ server/api/bookings/index.get.ts
@@ -1,10 +1,32 @@
+import { FieldPacket, RowDataPacket } from "mysql2/promise";
 import getConnection from "~/server/utils/db";
+import { BookingData, BookingStatus } from "~/types/BookingData";
 
 export default defineEventHandler(async (event) => {
     const query = getQuery(event);
     const db = await getConnection();
-
-    const result = await db.query("SELECT * FROM borrow_request")
     
+    const status = query.status as BookingStatus | undefined;
+    
+    let result: [BookingData[], FieldPacket[]];
+    if (!status) {
+        result = await db.query<BookingData[] & RowDataPacket[]>("SELECT * FROM borrow_request");
+    } 
+    else {
+        switch (status) {
+            case BookingStatus.pending:
+                result = await db.query<BookingData[] & RowDataPacket[]>("SELECT * FROM borrow_request WHERE status = 'pending'");
+                break;
+            case BookingStatus.accepted:
+                result = await db.query<BookingData[] & RowDataPacket[]>("SELECT * FROM borrow_request WHERE status = 'accepted'");
+                break;
+            case BookingStatus.rejected:
+                result = await db.query<BookingData[] & RowDataPacket[]>("SELECT * FROM borrow_request WHERE status = 'rejected'");
+                break;
+            default:
+                result = await db.query<BookingData[] & RowDataPacket[]>("SELECT * FROM borrow_request");
+        }
+    }
+   
     return result[0];
-})
+});
