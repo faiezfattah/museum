1b7b20168c6be2cce6836e9768b5223acef0f7d8
Faiez Fattah Wisnubrata <faiezfattah2005@gmail.com>
2025-04-08 11:39:08 +0700

    style fix

--- components/pageComposit/Collection/SectionThree.vue
+++ components/pageComposit/Collection/SectionThree.vue
@@ -8,10 +8,10 @@ const props = defineProps<{
   <section class="container">
     <h2 class="text-3xl font-bold text-center mb-8">Koleksi Kami</h2>
     <div class="grid grid-cols-3 gap-4">
-      <div v-for="(image, index) in images" :key="index" class="relative overflow-hidden">
+      <div v-for="(image, index) in images" :key="index" class="relative">
         <img 
           :src="image" 
-          class="w-full aspect-square object-cover border hover:scale-110 hover:z-10 hover:shadow-lg hover:shadow-white transition-transform duration-200" 
+          class="w-full aspect-square object-cover border-4 rounded-xl hover:scale-110 hover:z-10 hover:shadow-lg hover:shadow-white transition-transform duration-200" 
         />
       </div>
     </div>
